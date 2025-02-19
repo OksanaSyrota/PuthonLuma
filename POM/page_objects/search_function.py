bc3706844e62a1c9f1f637e7a69ce21e965e6720
oksanayakymets <yaoksana23@gmail.com>
2025-02-19 10:46:17 -0800

    Update

--- POM/page_objects/search_function.py
+++ POM/page_objects/search_function.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable, visibility_of_element_located, \
     presence_of_all_elements_located, text_to_be_present_in_element
+from sockshandler import is_ip
+
 from POM.page_objects.base_object import BaseObject
 from selenium.webdriver.common.by import By
 
@@ -8,6 +10,11 @@ from selenium.webdriver.common.by import By
 locators = {
     'SearchField': (By.CSS_SELECTOR, '#search'),
     'ConfirmSearchIcon': (By.XPATH, '//*[@class="action search"]'),
+    'SearchResultTitle': (By.CSS_SELECTOR, '.base'),
+    #'ProductsListSearchResult': (By.XPATH, '//*[@class="products list items product-items"]/li')
+    'ProductsListSearchResult': (By.CSS_SELECTOR, '.product-image-photo'),
+    'NoResultText': (By.XPATH, '//*[@class="message notice"]'),
+    'PartSearchResultsList': (By.XPATH, '//*[@class="block"]/dd/a'),
 
 }
 
@@ -15,6 +22,53 @@ class SearchFunction(BaseObject):
 
 
     #Search for an existing product/item and verify correct results are displayed.
-    def click_empty_cart_icon(self):
-        self.driver.find_element(*locators['CartIcon']).click()
-        self.wait.until(element_to_be_clickable(locators['CloseIconMinicart']))
+    def enter_text_in_search_field(self, text_search):
+        self.driver.find_element(*locators['SearchField']).send_keys(text_search)
+
+    def click_confirm_search_icon(self):
+        self.wait.until(element_to_be_clickable(locators['ConfirmSearchIcon']))
+        self.driver.find_element(*locators['ConfirmSearchIcon']).click()
+        self.wait.until(visibility_of_element_located(locators['SearchResultTitle']))
+        return self.driver.find_element(*locators['SearchResultTitle']).text
+
+    def check_list_of_items(self, word1, word2):
+        items = self.driver.find_elements(*locators['ProductsListSearchResult'])
+        titles = [item.get_attribute('alt') for item in items]
+        words_to_check = [word1, word2]
+        is_present = any(word in title for title in titles for word in words_to_check)
+        print(is_present)
+
+    def the_first_item_title(self, expected_title):
+        items = self.driver.find_elements(*locators['ProductsListSearchResult'])
+        if items and items[0].get_attribute('alt') == expected_title:
+            return items[0].get_attribute('alt')
+
+    #search for a Non-Existing Item
+    def get_no_search_result_text(self):
+        self.wait.until(visibility_of_element_located(locators['NoResultText']))
+        return self.driver.find_element(*locators['NoResultText']).text.split("\n")[0]
+
+    #Partial search input
+    def check_list_of_items_with_partial_input(self, partial_input):
+        items = self.driver.find_elements(*locators['ProductsListSearchResult'])
+        titles = [item.get_attribute('alt') for item in items]
+        is_present = (word in title for title in titles for word in partial_input)
+        print(is_present)
+
+    def check_related_search_terms_list(self, partial_input):
+        items = self.driver.find_elements(*locators['PartSearchResultsList'])
+        titles = [item.get_attribute('href') for item in items]
+        is_present = (word in title for title in titles for word in partial_input)
+        print(is_present)
+
+    def get_search_results(self, search_query):
+        search_field = self.driver.find_element(*locators['SearchField'])
+        search_field.clear()
+        search_field.send_keys(search_query)
+        self.wait.until(element_to_be_clickable(locators['ConfirmSearchIcon']))
+        self.driver.find_element(*locators['ConfirmSearchIcon']).click()
+        self.wait.until(visibility_of_element_located(locators['ProductsListSearchResult']))
+        items = self.driver.find_elements(*locators['ProductsListSearchResult'])
+        return [item.get_attribute('alt') for item in items]
+
+
--- POM/test_suits/SearchFunction.py
+++ POM/test_suits/SearchFunction.py
@@ -19,10 +19,39 @@ class SearchFunctionTestSuits(unittest.TestCase):
         cls.driver.maximize_window()
         cls.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#search')))
 
-    def test_open_empty_minicart(self):
-        self.header_navigation.click_empty_cart_icon()
-        self.assertEqual('You have no items in your shopping cart.',
-                         self.header_navigation.get_empty_minicart_text())
+    def test_search_valid_item_title(self):
+        self.search_function.enter_text_in_search_field('Layla Tee')
+        self.assertEqual("Search results for: 'Layla Tee'", self.search_function.click_confirm_search_icon())
+        #check if search list of items have at least one word from entered text
+        self.search_function.check_list_of_items('Layla', 'Tee')
+        #check if the first item is the item from entered text in the search field
+        self.assertEqual('Layla Tee', self.search_function.the_first_item_title('Layla Tee'))
+
+    #Search for a Non-Existing Item
+    def test_search_for_a_Non_Existing_Item(self):
+        self.search_function.enter_text_in_search_field('Test')
+        self.assertEqual("Search results for: 'Test'", self.search_function.click_confirm_search_icon())
+        self.assertEqual("Your search returned no results.", self.search_function.get_no_search_result_text())
+
+    #Partial Search Input
+    def test_partial_search_input(self):
+        self.search_function.enter_text_in_search_field('Sho')
+        self.assertEqual("Search results for: 'Sho'", self.search_function.click_confirm_search_icon())
+        #check if search list of items have partial input
+        self.search_function.check_list_of_items_with_partial_input('Sho')
+        #check if related search terms have partial input
+        self.search_function.check_related_search_terms_list('Sho')
+
+    #Case Insensitivity (Search with different cases ("t shirt" vs. "t-shirt") and verify that results remain the same)
+    def test_case_insensitivity(self):
+        result1 = self.search_function.get_search_results('t shirt')
+        result2 = self.search_function.get_search_results('t-shirt')
+        if result1 == result2:
+            print("Both searches returned the same list of items.")
+
+
+
+
 
 
 
